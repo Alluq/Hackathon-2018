251a7522d956fe2a5803b86e18f9b3b09a1c38c2
AlkalineBattery <39013427+ElBattery@users.noreply.github.com>
2018-05-25 09:59:16 -0400

    Update SoundBoardScreen.py

--- SoundBoardScreen.py
+++ SoundBoardScreen.py
@@ -1,14 +1,19 @@
 import utility
 import random
+import pygame
+
+
 
 class Sound_Board:
     def __init__(self, screenSize, screen):
         self.screen = screen
         self.screenSize = screenSize
         self.buttons = []
-        for i in range(3):
-            for j in range(5):
-                self.buttons.append(utility.Button(self.screen,(i*self.screenSize[0]/5,j*screenSize[1]/5,self.screenSize[0]/5,self.screenSize[1]/5),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),Text(self.screen,0,0,"Woah",pygame.font.SysFont("Comic Sans MS",25),(random.randint(0,255),random.randint(0,255),random.randint(0,255)))))
+        for i in range(5):
+            for j in range(3):
+                self.buttons.append(utility.Button(self.screen,(i*self.screenSize[0]/5,j*screenSize[1]/5,self.screenSize[0]/5,self.screenSize[1]/5),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),(random.randint(0,255),random.randint(0,255),random.randint(0,255)),
+                                                   utility.Text(self.screen,0,0,"WOAH",pygame.font.SysFont("comicsansms",25),(255,255,255))))
     def update(self):
         for i in self.buttons:
+            i.hoverChange()
             i.draw()
